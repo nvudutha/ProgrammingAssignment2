1270fa6082cf3912bdceec7c8273188500d41e8f
Narasimha Gupta Vudutha <vnarsim@hotmail.com>
2018-02-13 18:14:31 +0530

    Logic added to the functions
    Logic added to the Functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create and cache the inverse of a given matrix
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # set inv to NULL 
+        inv <- NULL
+        
+        # store matrix
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        setInverse <- function(solveMatrix) inv <<- solveMatrix
+        getInverse <- function() inv
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special matrix returned by makeCacheMatrix
+## if the inverse already exist,then cachesolve retrieves inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## get the cached value 
+        inv <- x$getInverse()
+        
+        ## if cached value exist, return it
+        if (!is.null(inv)){
+                message ("retrieving cached data")
+                return(inv)
+        }
+        ## if cached value does not exist, calculate the inverse and cache it
+        data <- x$get()
+        inv <- solve(data)
+        x$setInverse(inv)
+        inv
 }
